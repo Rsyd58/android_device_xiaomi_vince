f1efe3ccb127dd347e4ba06a480af7ca8427accd
Suren Baghdasaryan <surenb@google.com>
2022-07-18 17:06:06 -0700

    vince: Disable per-cgroup PSI accounting
    Per-cgroup PSI accounting is not used in Android while having small
    performance cost. Disable this unused feature.
    
    Bug: 233410456
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ib5174d7d819e33e2fc2f71bfafb68e8f4b79631a
    Merged-In: Ib5174d7d819e33e2fc2f71bfafb68e8f4b79631a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ RELAX_USES_LIBRARY_CHECK := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 cgroup_disable=pressure
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
