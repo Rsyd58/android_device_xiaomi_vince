890ebf34b5571b4ac59cee5cd8cc2ca7e1fc5484
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-09-21 09:14:27 +0000

    vince: Inherit from QTI FCM
    *Also Include lineage FCM & Inherit from legacy QTI FCM
    
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,10 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
