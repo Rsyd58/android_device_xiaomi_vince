fd3292766083013def4996615b93f350909aa7bb
Citlali-Itztli <citlaliofitztli@gmail.com>
2024-12-22 16:21:11 +0000

    vince: BoardConfig: EGL: configure shader cache
    *Taken from vendor/qcom/titanium_64/BoardConfig.mk
    
    Signed-off-by: Citlali-Itztli <citlaliofitztli@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,16 @@ TARGET_USES_ION := true
 TARGET_USES_GRALLOC1 := true
 TARGET_USES_HWC2 := true
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 3.0
