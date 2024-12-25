bca18e6c2d3dd90e6a8d58583ca3ae536f7bcef0
Citlali <citlaliofitztli@gmail.com>
2024-12-25 04:28:38 +0000

    vince: bringup for RisingTechOSS

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -15,6 +15,18 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
+# Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+RISING_MAINTAINER := CitlaliItztli
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+WITH_GMS := false
+RISING_BUILDTYPE := UNOFFICIAL
+PRODUCT_NO_CAMERA := false
+TARGET_SHIP_LINEAGE_PREBUILTS := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
 PRODUCT_NAME := lineage_vince
@@ -27,6 +39,8 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingMaintainer="CitlaliItztli" \
+    RisingChipset="Snapdragon 625" \
     BuildDesc="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys" \
     BuildFingerprint=xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys \
     DeviceProduct=vince
